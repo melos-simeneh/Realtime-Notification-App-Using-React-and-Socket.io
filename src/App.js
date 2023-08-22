4a0681f7da4f7e6829d6a29f735d2c4778a04b07
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-22 15:00:39 -0700

    Add navbar and card to app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import { useState } from "react";
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Card from "./components/card/Card";
 
 function App() {
   const [username, setUsername] = useState("");
   const [user, setUser] = useState("");
   return (
     <div className="container">
-      <div className="login">
-        <input
-          type="text"
-          placeholder="username"
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <button onClick={() => setUser(username)}>Login</button>
-      </div>
+      {user ? (
+        <>
+          <Navbar />
+          <Card />
+        </>
+      ) : (
+        <div className="login">
+          <input
+            type="text"
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button onClick={() => setUser(username)}>Login</button>
+        </div>
+      )}
     </div>
   );
 }
