772191c972ffd6f53f5eff7b374400f82c0d7b7a
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-22 16:56:23 -0700

    add,remove and get user to/from online user list

--- socket/index.js
+++ socket/index.js
@@ -6,8 +6,22 @@ const io = new Server({
   },
 });
 
+let onlineUsers = [];
+
+const addNewUser = (username, socketId) => {
+  !onlineUsers.some((user) => username === username) &&
+    onlineUsers.push({ username, socketId });
+};
+
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
+
+const getuser = (username) => {
+  return onlineUsers.find((user) => user.username === username);
+};
+
 io.on("connection", (socket) => {
-  console.log("someone has connected!");
   socket.on("disconnect", () => {
     console.log("someone has left");
   });
