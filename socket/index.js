71bd5f1eb9fed30f4d8815b52ea1f6de8d089c7b
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-23 10:34:49 -0700

    send,get notification and text

--- socket/index.js
+++ socket/index.js
@@ -1,4 +1,4 @@
-const Server = require("socket.io");
+const { Server } = require("socket.io");
 
 const io = new Server({
   cors: {
@@ -9,7 +9,7 @@ const io = new Server({
 let onlineUsers = [];
 
 const addNewUser = (username, socketId) => {
-  !onlineUsers.some((user) => username === username) &&
+  !onlineUsers.some((user) => user.username === username) &&
     onlineUsers.push({ username, socketId });
 };
 
@@ -17,14 +17,37 @@ const removeUser = (socketId) => {
   onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
 };
 
-const getuser = (username) => {
+const getUser = (username) => {
   return onlineUsers.find((user) => user.username === username);
 };
 
 io.on("connection", (socket) => {
+  console.log("Someone connected");
+  socket.on("newUser", (username) => {
+    console.log(onlineUsers);
+    addNewUser(username, socket.id);
+  });
+
+  socket.on("sendNotification", ({ senderName, receiverName, type }) => {
+    const receiver = getUser(receiverName);
+    io.to(receiver.socketId).emit("getNotification", {
+      senderName,
+      type,
+    });
+  });
+
+  socket.on("sendText", ({ senderName, receiverName, text }) => {
+    const receiver = getUser(receiverName);
+    io.to(receiver.socketId).emit("getText", {
+      senderName,
+      text,
+    });
+  });
+
   socket.on("disconnect", () => {
-    console.log("someone has left");
+    removeUser(socket.id);
   });
 });
 
-io.listen(8001);
+const PORT = 8001;
+io.listen(PORT);
